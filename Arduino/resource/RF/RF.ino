fdb25df74d94b442d7a1b3b30d05f5bb4b92a18c
Hanoch Griner <eugriner@gmail.com>
2015-11-11 17:05:49 -0300

    RF example modify

--- Arduino/resource/RF/RF.ino
+++ Arduino/resource/RF/RF.ino
@@ -1,18 +1,26 @@
 /**
  * An Mirf example which copies back the data it recives.
  *
+ * 1-GND
+ * 2-3.3V
+ * 3-CE
+ * 4-CSN
+ * 5-SCK
+ * 6-MOSI
+ * 7-MISO
+ * 8-IRQ
+ *
  * Pins:
  * Hardware SPI:
- * MISO -> 12
- * MOSI -> 11
- * SCK -> 13
+ * 7-MISO -> 12
+ * 6-MOSI -> 11
+ * 5-SCK  -> 13
  *
  * Configurable:
- * CE -> 8
- * CSN -> 7
+ * 3-CE   -> 9
+ * 4-CSN  -> 10
  *
  */
-#include <SoftwareSerial.h>
 #include <SPI.h>
 #include <Mirf.h>
 #include <nRF24L01.h>
@@ -20,6 +28,8 @@
 
 // Variable to hold input and output char of messages
 char str;
+char myAddress[] = "serv1";
+char destinAddress[] = "serv2";
 
 void setup(){
   Serial.begin(9600);
@@ -28,7 +38,8 @@ void setup(){
    * Set the SPI Driver.
    */
   Mirf.spi = &MirfHardwareSpi;
-  
+  Mirf.cePin = 9;
+  Mirf.csnPin = 10;
   /*
    * Setup pins / SPI.
    */
@@ -37,7 +48,7 @@ void setup(){
   /*
    * Configure reciving address.
    */
-  Mirf.setRADDR((byte *)"serv1");
+  Mirf.setRADDR((byte *)myAddress);
   
   /*
    * Set the payload length, payload on client and server must be the same.
@@ -57,6 +68,7 @@ void setup(){
    * Write config then power up reciver.
    */
   Mirf.config();
+  Mirf.setTADDR((byte *)destinAddress);
 
   Serial.println("Ready..."); 
 }
@@ -70,10 +82,10 @@ void loop(){
 
   // Read and Send
   if(Serial.available()) {
-    Mirf.setTADDR((byte *)"serv2");
+    // Read char from serial
     str = Serial.read();
     
-    // Send
+    // Send char
     Mirf.send((byte *)&str);
     
     // Wait everything be sent
